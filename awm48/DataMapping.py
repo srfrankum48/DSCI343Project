5d94a7210fa8a41a74ca9b2a241c19d13e0a9675
AlexWesMarshall <alex.monkey.marshall@gmail.com>
2019-11-11 14:04:22 -0500

    Added generic method for pulling crimes by code
    New york has a method for making a new csv file based on a crime code

--- awm48/DataMapping.py
+++ awm48/DataMapping.py
@@ -9,6 +9,12 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+#CSVs
+NYcsv = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\NewYorkCrimes.csv'
+SFcsv = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\SanFranciscoCrimes.csv'
+LAcsv = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\LosAngelesCrimes.csv'
+BOcsv = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\BostonCrimes.csv'
+
 #Key words to search by
 ASSAULT = "assault"
 BURGLARY = "burglary"
@@ -17,25 +23,27 @@ LACERNY = "lacerny"
 DRUG = "drug"
 
 #New York
-NYCrimes = []
-NYCrimesMonth2013 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-NYCrimesMonth2014 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-NYCrimesMonth2015 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-with open('C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\NewYorkCrimes.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        if ASSAULT in row[9].lower():
-            date = row[1]
-            try:
-                month = int(date[:2].replace("/",""))
-            except:
-                print(date)
-            else:
-                year = int(date[-4:])
-                if year == 2014 | year == 2015:
-                    NYCrimesMonth2014[month] = NYCrimesMonth2014[month] + 1
-                NYCrimesMonth2015[month] = NYCrimesMonth2015[month] + 1
 
+def getNYCrimesByCode(pdCode, csvFileLoc):
+    crimes = []
+    with open(csvFileLoc,'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        crimes.append(next(plots))
+        for row in plots:
+            try:
+                rowPDCode = int(row[8])
+            except:
+                1 + 1
+            else:
+                if rowPDCode == pdCode: #row[8] is pdcode in NYCrimes.csv
+                    crimes.append(row)
+        csvLoc = 'C:\\Users\\Regina\\Documents\\GitHub\\DSCI343Project\\awm48\\SortedData\\NewYorkCrimeCode' + str(pdCode) + '.csv'
+        with open(csvLoc, 'w', newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerows(crimes)
+
+getNYCrimesByCode(333, NYcsv)
+'''
 data = NYCrimesMonth2014[1:13]
 print(data)
 bars = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec') 
@@ -54,7 +62,8 @@ SFCrimes16 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 SFCrimes17 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 SFCrimes18 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 SFCrimes19 = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-with open('C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\SanFranciscoCrimes.csv','r') as csvfile:
+
+with open(SFcsv,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         if ASSAULT in row[14].lower():
@@ -91,17 +100,39 @@ plt.bar(bars, sfdata)
 plt.title(label = 'Crimes per month in SF')
 plt.xticks(y_pos, bars)
 plt.show()
-
+'''
 #Los Angeles     
 LACrimes = []
-with open('C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\LosAngelesCrimes.csv','r') as csvfile:
+with open(LAcsv,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         LACrimes.append(0)
   
 #Boston      
 BOCrimes = []
-with open('C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\BostonCrimes.csv','r') as csvfile:
+with open(BOcsv,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         BOCrimes.append(0)
+        
+        
+        
+'''
+Old code for parsing date 
+def getNYCrimesByCode(pdCode, csvFileLoc):
+    crimes = []
+    with open(csvFileLoc,'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if pdCode == row[8]: #row[8] is pdcode in NYCrimes.csv
+                date = row[1] #row[1] is date and time
+                try:
+                    month = int(date[:2].replace("/",""))
+                    year = int(date[-4:])
+                except:
+                    print(date)
+                else:
+                    if year == 2014 | year == 2015:
+                        NYCrimesMonth2014[month] = NYCrimesMonth2014[month] + 1
+                    NYCrimesMonth2015[month] = NYCrimesMonth2015[month] + 1
+'''
