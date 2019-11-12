1f925f3b43bd530915f31189e5805f279c7321fc
AlexWesMarshall <alex.monkey.marshall@gmail.com>
2019-11-11 21:38:46 -0500

    DataMapping Script Finished
    Finished the data mapping script and left some example csv files

--- awm48/DataMapping.py
+++ awm48/DataMapping.py
@@ -6,8 +6,7 @@ Created on Sun Nov 9 19:03:22 2019
 Project Work with Data Mapping
 """
 import csv
-import numpy as np
-import matplotlib.pyplot as plt
+
 #CHANGE ME
 baseDirectory = 'C:\\Users\\Regina\\Documents\\GitHub\\DSCI343Project\\awm48\\SortedData\\'
 
@@ -20,109 +19,178 @@ COcsv1 = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Pro
 COcsv2 = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\ChicagoCrimes2005to2007.csv'
 COcsv3 = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\ChicagoCrimes2008to2011.csv'
 COcsv4 = 'C:\\Users\\Regina\\Documents\\Academics\\Fall 2019\\Data Analysis\\Project\\ChicagoCrimes2012to2017.csv'
-
-#Key words to search by
-ASSAULT = "assault"
-BURGLARY = "burglary"
-ROBBERY = "robbery"
-LACERNY = "lacerny"
-DRUG = "drug"
+COcsv = [COcsv1, COcsv2, COcsv3, COcsv4]
 
 #New York
-def getNYCrimesByCode(code, csvFileLoc):
+def getNYCrimesByCode(csvFileLoc, *codes):
     crimes = []
-    with open(csvFileLoc,'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        crimes.append(next(plots))
-        for row in plots:
-            try:
-                rowPDCode = int(row[8])
-            except:
-                print(row[8])
+    csvLoc = baseDirectory + "NewYorkCrimeCode"
+    headerFlag = True
+    for code in codes:
+        with open(csvFileLoc,'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            if headerFlag:
+                crimes.append(next(plots))
+                headerFlag = False
             else:
-                if rowPDCode == code: #row[8] is pdcode in NYCrimes.csv
-                    crimes.append(row)
-        csvLoc = baseDirectory + "NewYorkCrimeCode" + str(code) + '.csv'
-        with open(csvLoc, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(crimes)
+                next(plots)
+            for row in plots:
+                try:
+                    rowPDCode = int(row[8])
+                except:
+                    print(row[8])
+                else:
+                    if rowPDCode == code: #row[8] is pdcode in NYCrimes.csv
+                        crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)
 
-#getNYCrimesByCode(333, NYcsv)
+#getNYCrimesByCode(NYcsv, 333, 511)
+            
 #San Francisco
-def getSFCrimesByCode(code, csvFileLoc):
+def getSFCrimesByCode(csvFileLoc, *codes):
     crimes = []
-    with open(csvFileLoc,'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        crimes.append(next(plots))
-        for row in plots:
-            try:
-                rowPDCode = int(row[13])
-            except:
-                print(row[13])
+    csvLoc = baseDirectory + "SanFranciscoCrimeCode"
+    headerFlag = True
+    for code in codes:
+        with open(csvFileLoc,'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            if headerFlag:
+                crimes.append(next(plots))
+                headerFlag = False
             else:
-                if rowPDCode == code: #row[13] is category code in SFCrimes.csv
-                    crimes.append(row)
-        csvLoc = baseDirectory + "SanFranciscoCrimeCode" + str(code) + '.csv'
-        with open(csvLoc, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(crimes)
+                next(plots)
+            for row in plots:
+                try:
+                    rowPDCode = int(row[13])
+                except:
+                    print(row[13])
+                else:
+                    if rowPDCode == code: #row[13] is category code in SFCrimes.csv
+                        crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)
 
+#getSFCrimesByCode(SFcsv, 4134, 28160)
 
-#getSFCrimesByCode(4134, SFcsv)
 #Los Angeles     
-def getLACrimesByCode(code, csvFileLoc):
+def getLACrimesByCode(csvFileLoc, *codes):
     crimes = []
-    with open(csvFileLoc,'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        crimes.append(next(plots))
-        for row in plots:
-            try:
-                rowCode = int(row[7])
-            except:
-                print(row[7])
+    csvLoc = baseDirectory + "LosAngelesCrimeCode"
+    headerFlag = True
+    for code in codes:
+        with open(csvFileLoc,'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            if headerFlag:
+                crimes.append(next(plots))
+                headerFlag = False
             else:
-                if rowCode == code:
-                    crimes.append(row)
-        csvLoc = baseDirectory + "LosAngelesCrimeCode" + str(code) + '.csv'
-        with open(csvLoc, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(crimes)
+                next(plots)
+            for row in plots:
+                try:
+                    rowCode = int(row[7])
+                except:
+                    print(row[7])
+                else:
+                    if rowCode == code:
+                        crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)
  
-#getLACrimesByCode(510, LAcsv)
-#Boston      
-def getBostonCrimesByCode(code, csvFileLoc):
-    crimes = []
-    with open(csvFileLoc,'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        crimes.append(next(plots))
-        for row in plots:
-            try:
-                rowCode = int(row[1])
-            except:
-                print(row[1])
-            else:
-                if rowCode == code:
-                    crimes.append(row)
-        csvLoc = baseDirectory + "BostonCrimeCode" + str(code) + '.csv'
-        with open(csvLoc, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(crimes)              
+#getLACrimesByCode(LAcsv, 510, 626)
         
-#getBostonCrimesByCode(3301, BOcsv)
-#Chicago 
-def getChicagoCrimesByCode(code, csvFileLoc):
+#Boston      
+def getBostonCrimesByCode(csvFileLoc, *codes):
     crimes = []
-    with open(csvFileLoc,'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        crimes.append(next(plots))
-        for row in plots:
-            r = row[5]
-            if code == r:
-                crimes.append(row)
-        csvLoc = baseDirectory + "ChicagoCrimeCode" + str(code) + '.csv'
-        with open(csvLoc, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(crimes)
+    csvLoc = baseDirectory + "BostonCrimeCode"
+    headerFlag = True
+    for code in codes:
+        with open(csvFileLoc,'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            if headerFlag:
+                crimes.append(next(plots))
+                headerFlag = False
+            else:
+                next(plots)
+            for row in plots:
+                try:
+                    rowCode = int(row[1])
+                except:
+                    print(row[1])
+                else:
+                    if rowCode == code:
+                        crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)              
+        
+#getBostonCrimesByCode(BOcsv, 612, 613)
+        
+#Chicago 
+def getChicagoCrimesByCode(csvFileLoc, *codes):
+    crimes = []
+    csvLoc = baseDirectory + "ChicagoCrimeCode"
+    headerFlag = True
+    for code in codes:
+        with open(csvFileLoc,'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            if headerFlag:
+                crimes.append(next(plots))
+                headerFlag = False
+            else:
+                next(plots)
+            for row in plots:
+                try:
+                    r = row[5]
+                except:
+                    print(row[5])
+                else:
+                    if r == code:
+                        crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)
+        
+def getChicagoCrimesByCodeAllCSVs(*codes):
+    crimes = []
+    csvLoc = baseDirectory + "ChicagoCrimeCode"
+    headerFlag = True
+    for code in codes:
+        for c in COcsv:
+            with open(c,'r') as csvfile:
+                plots = csv.reader(csvfile, delimiter=',')
+                if headerFlag:
+                    crimes.append(next(plots))
+                    headerFlag = False
+                else:
+                    next(plots)
+                for row in plots:
+                    try:
+                        r = row[5]
+                    except:
+                        print(row[5])
+                    else:
+                        if r == code:
+                            crimes.append(row)
+        csvLoc += '_' + str(code)
+    csvLoc += '_From2001.csv'
+    with open(csvLoc, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(crimes)
 
 #code for Chicago must be 4 characters long or it won't work           
-getChicagoCrimesByCode("0486", COcsv4)
+#getChicagoCrimesByCode(COcsv4, "0486", "0820")
+#getChicagoCrimesByCodeAllCSVs("0486")
